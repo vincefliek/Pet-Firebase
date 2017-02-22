6cf91788e1ead14ea7d6db7136684abb07fc7997
Vince Fliek <vincefliek@gmail.com>
2017-02-22 22:13:59 +0200

    Test ssh

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ const ENV_DEVELOPMENT = NODE_ENV === 'development';
 const ENV_PRODUCTION = NODE_ENV === 'production';
 const ENV_TEST = NODE_ENV === 'test';
 
-// ----------------------
 const HOST = '0.0.0.0';
 const PORT = 3000;
 
