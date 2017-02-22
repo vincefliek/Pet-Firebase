2d45064944d4ce975247148e902179301f813458
Vince Fliek <vincefliek@gmail.com>
2017-02-22 21:49:14 +0200

    Initial

---
+++ src/core/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZ-XO9PPVdPYvydsT_pn2x9C0RJcLPHJE",
+  authDomain: "pet-sticks.firebaseapp.com",
+  databaseURL: "https://pet-sticks.firebaseio.com",
+  storageBucket: "pet-sticks.appspot.com",
+  messagingSenderId: "1045011213965"
+};
